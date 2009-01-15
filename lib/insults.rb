f4f543190323c7f98ad79d7a83693733a9138bac
delano <delano.mandelbaum@gmail.com>
2009-01-15 15:05:09 -0500

    Cleanup and more insults

--- lib/insults.rb
+++ lib/insults.rb
@@ -21,50 +21,78 @@
 $KCODE = "u"
 require 'yaml'
 
-@@text = YAML.load(<<-text)
---- 
-:fuckups: 
-- "Ybbx vqvbg, '%f' qbrfa'g rkvfg. "
-- Url fuvg svatref, gurer vf ab '%f'.
-:includes: 
-- "%f qbrfa'g arrq %f, jung gur uryy ner lbh guvaxvat?"
-- Qb lbh ernyyl guvax lbhe %f jvyy qb nal orggre guna %f
-- Bxnl, fb %f vaurevgrq %f. Abj jung?
-text
-
-def fuckup #:nodoc:
-  @@text[:fuckups][rand(@@text[:fuckups].size)]; 
-end
-def shiton #:nodoc:
-  @@text[:includes][rand(@@text[:includes].size)]; 
-end
-
-eval(<<-eos, binding)
-
-  def _(you, *are, &stupid); ✖ fuckup, you; end
-  def ✖(w,*m); STDERR.puts w.tr("A-Za-z", "N-ZA-Mn-za-m") % m; end
-  %w[ ⚀ ⚁ ⚂ ⚃ ⚄ ⚅ ].each do |die|
-    self.class.send(:define_method, die) { |*m| rand >= (die.unpack('C*')[2]-127)/10; }
-  end
- 
-  class Object
-    def self.inherited(refelection)
-      ⚃ and ✖ shiton, self, refelection unless self == Object
-    end
-  end
-#  ✖(,*m)
-  class Module
-    define_method(:const_missing) {     |you|       ✖ fuckup, you }
-    define_method(:extended)      {  |fucked| ⚄ and ✖ shiton, self, fucked }
-    define_method(:included)      {      |it| ⚄ and ✖ shiton, self, it } 
-  end
-  
-  alias :method_missing :"#{"YOUR_FACE".unpack("C*").inject(0){|f,u|f|u}.chr}"
-eos
-
 # There are no methods or constants. All you have to do is require insults and 
 # it will insult you when it thinks you deserve it.
 # <p>TODO: add more insults.</p>
 module Insults
 end
 
+class Insult < Array #:nodoc:all
+  def initialize(world_of_pain)
+    push(YAML.load(world_of_pain)).flatten!
+  end
+  def decide
+    self[rand(self.length)] || decide
+  end
+end
+
+class String
+  alias :DIRTY :unpack
+end
+class Array
+  alias :SUCKER :inject
+end
+
+eval(<<-eos, binding)
+
+  %w[ ⚀ ⚁ ⚂ ⚃ ⚄ ⚅ ].each do |die|
+    self.class.send(:define_method, die) { |*m| rand >= (die.unpack('C*')[2]-127.0)/10; }
+  end
+  
+  def ✖(w,*m); STDERR.puts w.tr("A-Za-z", "N-ZA-Mn-za-m") % m; end
+  def _(again, *tobeless, &stupid); ✖ fuckup.decide, again; end
+  
+  class Module
+    define_method(:const_missing) {     |you|       ✖ fuckup.decide, you }
+    define_method(:extended)      {  |fucked| ⚄ and ✖ shiton.decide, self, fucked }
+    define_method(:included)      {      |it| ⚄ and ✖ shiton.decide, self, it } 
+  end
+  
+  class Object
+    self.class.send(:define_method, :inherited) { |r| ⚃ and ✖ shiton.decide, self, r unless self == Object }
+  end
+  
+  
+  alias :method_missing :"#{"YOU_ARE_A".DIRTY("C**K").SUCKER(0){|f,u|f|u}.chr}"
+
+
+  def shiton 
+    Insult.new(<<-text)
+    - Qb lbh ernyyl guvax lbhe "%f" jvyy qb nal orggre guna %f
+    - Bxnl, fb "%f" vaurevgrq "%f". Abj jung?
+    - Jul ner lbh fhpu n qvpxyvpxre?
+    - Lrf V qb xabj jung lbh zrna ol "chyy erdhrfg" naq V qba'g yvxr vg.
+    - Lbh ybir obarfgbezf qba'g lbh.
+    - Jryy, jr obgu xabj gung lbh fhpx. Gur bayl dhrfgvba vf ubj bsgra. 
+    - Abobql yvxrf lbh.
+    - Guvf vf n svanapvny naq phygheny qvfnfgre. 
+    - V guvax lbh'er fghcvq, "%f" guvaxf lbh'er fghcvq, naq "%f" guvaxf lbh'er fghcvq.
+    text
+  end
+
+  def fuckup
+    Insult.new(<<-text)
+    - Ybbx vqvbg, "%f" qbrfa'g rkvfg. 
+    - Url fuvg svatref, gurer vf ab "%f".
+    - Lbhe pbqr fzryyf yvxr n ont bs qvpxf. Nyfb, "%f" qbrfa'g rkvfg.
+    - Url qvpx juvfcrere, "%f" qbrfa'g rkvfg!
+    - Vg'f bxnl gb fhpx, nf ybat nf lbh'er gelvat lbhe orfg.
+    - Fuhg gur shpx hc Qbaal!
+    - Lbhe Qnq pnyyrq. Ur'f jbaqrevat jura lbh'er tbvat gb npprcg uvf chyy erdhrfg.
+    text
+  end
+
+eos
+
+
+srand
